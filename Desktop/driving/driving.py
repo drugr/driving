f4236bd3f430ccd3360bbb3db0cd892794411bdf
Kunn Liu <abce1202@gmail.com>
2019-10-09 19:59:05 +0800

    add japan part

--- Desktop/driving/driving.py
+++ Desktop/driving/driving.py
@@ -11,7 +11,14 @@ elif country == '美國':
 		print('你可以考駕照囉')
 	else:
 		print('你還不能考駕照')	
+elif country == '日本':
+	if age < 16:
+		print('你不能考駕照喔')
+	elif age >=16 and age < 18:
+		print('雖然你不能考汽車駕照，但可以考普通二輪機車駕照喔')
+	else:
+		print('你可以考駕照了')
 
 
 else:
-	print('請輸入"台灣" "美國"，而且字要一模一樣喔!!!')
+	print('請輸入"台灣" "美國" "日本"，而且字要一模一樣喔!!!')
