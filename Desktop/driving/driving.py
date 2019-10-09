fa961318b5b0b12ba529290dbba0321dac26624c
Kunn Liu <abce1202@gmail.com>
2019-10-09 19:38:50 +0800

    add the USA

--- Desktop/driving/driving.py
+++ Desktop/driving/driving.py
@@ -1,8 +1,17 @@
 country = input('你是哪國人: ')
 age = input('請輸入年齡: ')
 age = int(age)
-if country == '台灣' or '臺灣' or 'Taiwan':
+if country == '台灣':
 	if age >= 18:
 		print('你可以考駕照囉')
 	else:
 		print('你還不能考駕照')
+elif country == '美國':
+	if age >= 16:
+		print('你可以考駕照囉')
+	else:
+		print('你還不能考駕照')	
+
+
+else:
+	print('請輸入"台灣" "美國"')
